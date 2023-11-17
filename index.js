97e15fc0e7df400b7529735679c34feeb642e061
sarahlynnx <xsarahlynn06x@gmail.com>
2023-11-16 17:46:39 -0800

    send email feature, responsive

--- index.js
+++ index.js
@@ -1,24 +1,15 @@
-$(document).ready(function () {
-    $("a").on('click', function (event) {
-      if (this.hash !== "") {
-        event.preventDefault();
 
-        var hash = this.hash;
+//skills bubbles positioning  
+document.addEventListener("DOMContentLoaded", function () {
+  const buttons = document.querySelectorAll(".disabled-buttons button");
 
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top - 70
-        }, 800, function () {
-          window.location.hash = hash;
-        });
-      }
-    });
+  buttons.forEach((button, index) => {
+      const angle = (index / buttons.length) * 360; 
+      const radius = 30; 
+      const x = radius * Math.cos((angle * Math.PI) / 180);
+      const y = radius * Math.sin((angle * Math.PI) / 180);
 
-    $('i').hover(
-      function () {
-          $(this).animate({ marginTop: '-10px' }, 200);
-      },
-      function () {
-          $(this).animate({ marginTop: '0px' }, 200);
-      }
-  );
+      button.style.left = `${40 + x}%`; 
+      button.style.top = `${40 + y}%`; 
+  });
 });
